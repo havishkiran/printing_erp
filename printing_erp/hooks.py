5dae8a48deb45c19641b092193a8101bcd6d67e2
havishkiran <55433526+havishkiran@users.noreply.github.com>
2025-06-22 12:19:12 +0530

    Update hooks.py

--- printing_erp/hooks.py
+++ printing_erp/hooks.py
@@ -1,6 +1,7 @@
-app_name = 'printing_erp'
-app_title = 'Printing ERP'
-app_publisher = 'Your Name'
-app_email = 'you@example.com'
-app_license = 'MIT'
-fixtures = ['Custom Field']
+app_name = "printing_erp"
+app_title = "Printing ERP"
+app_publisher = "Your Name"
+app_description = "Custom app for print job quoting, pricing, and work orders."
+app_email = "you@example.com"
+app_license = "MIT"
+fixtures = ["Custom Field"]
